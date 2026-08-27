4c5c31317a243444fe77e42ae471fb87f97e4976
JukLee0ira <meebookMonkey@163.com>
2026-08-27 23:18:11 +0800

    fix(cors): allow production payment origins

--- huizhipay-bootstrap/src/main/java/com/huizhipay/bootstrap/config/CorsConfig.java
+++ huizhipay-bootstrap/src/main/java/com/huizhipay/bootstrap/config/CorsConfig.java
@@ -20,7 +20,9 @@ public class CorsConfig {
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
                 "http://localhost:8080",
-                "http://127.0.0.1:8080"
+                "http://127.0.0.1:8080",
+                "https://huizhipay.org",
+                "https://www.huizhipay.org"
         ));
         
         // 允许的HTTP方法
